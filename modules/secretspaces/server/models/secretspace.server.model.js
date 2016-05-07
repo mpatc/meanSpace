a07c12309f0184fa6e2f15bfbc386284569462eb
mpatc <matthewpclancy@gmail.com>
2016-05-07 11:38:26 -0700

    change users/../default.png

--- modules/secretspaces/server/models/secretspace.server.model.js
+++ modules/secretspaces/server/models/secretspace.server.model.js
@@ -38,3 +38,14 @@ var SecretspaceSchema = new Schema({
 });
 
 mongoose.model('Secretspace', SecretspaceSchema);
+function getMissingNumbers(numbers) {
+var list = [0,1,2,3,4,5,6,7,8,9];
+for (var i = 0; i < numbers.length; i++) {
+    for (var j = 0; j < list.length; j++) {
+        if (numbers[i] === list[j]) {
+            list.splice(j, 1);
+}
+}
+}
+    return list;
+}
